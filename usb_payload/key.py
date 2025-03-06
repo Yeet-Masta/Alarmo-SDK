8087f6d71f73cbbad0d2021125392f74dd92c1c8
Yeet-Masta <yeetmasta234@gmail.com>
2025-03-06 14:48:49 -0500

    nuh-uhh
    ...

--- usb_payload/key.py
+++ usb_payload/key.py
@@ -1,2 +1,2 @@
-AES_KEY = bytearray.fromhex("284f6779801cba309ece7968e48f1e26")
-AES_IV = bytearray.fromhex("d12b38396bd19b52ce803a8f00000000")
+AES_KEY = bytearray.fromhex("hnrukujktyjtyhjn")
+AES_IV = bytearray.fromhex("wedfsefwrgetger")
